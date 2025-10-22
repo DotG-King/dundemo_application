eb859cc3db9bc73510df0a187bde40da1a5d3080
류준현 <tms02171@gmail.com>
2025-10-22 16:28:03 +0900

    init: git init

---
+++ src/main/java/dev/example/dundemo/DundemoApplication.java
@@ -0,0 +1,13 @@
+package dev.example.dundemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DundemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DundemoApplication.class, args);
+    }
+
+}
