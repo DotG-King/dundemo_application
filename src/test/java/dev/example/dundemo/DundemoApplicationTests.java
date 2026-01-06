c89fa5c3b2252d1a4c1c298dc7892dab71cb0137
류준현 <tms02171@gmail.com>
2026-01-06 15:54:42 +0900

    Feat: 테스트 빌드시 내장 몽고DB 사용하도록 명시

--- src/test/java/dev/example/dundemo/DundemoApplicationTests.java
+++ src/test/java/dev/example/dundemo/DundemoApplicationTests.java
@@ -2,8 +2,10 @@ package dev.example.dundemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.data.mongodb.uri=mongodb://localhost/test")
 class DundemoApplicationTests {
 
     @Test
